833fb2724ac197845846790bdda2e85056c98a3f
Alexander Matlin <ratingsoft@gmail.com>
2020-12-31 18:20:29 +0100

    Added a Binary Search (Recursive) algorithm.

--- src/search/Search.java
+++ src/search/Search.java
@@ -10,4 +10,28 @@ public class Search {
 
 		return -1;
 	}
+
+	public int binarySearch(int[] array, int target) {
+		return binarySearchRecursive(array, target, 0, array.length - 1);
+	}
+
+	private int binarySearchRecursive(int[] array, int target, int left, int right) {
+		// Base condition
+		if (right < left) {
+			return -1;
+		}
+
+		int middle = (left + right) / 2;
+		if (array[middle] == target) {
+			return middle;
+		}
+
+		// Searching the target in the left part
+		if (target < array[middle]) {
+			return binarySearchRecursive(array, target, left, middle - 1);
+		}
+
+		// Searching the target in the right part
+		return binarySearchRecursive(array, target, middle + 1, right);
+	}
 }

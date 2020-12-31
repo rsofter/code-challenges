3a6a96a72423feac81b5069f02c3f4eea52a9f97
Alexander Matlin <ratingsoft@gmail.com>
2020-12-31 17:41:29 +0100

    Added a Linear Search algorithm.

---
+++ src/search/Search.java
@@ -0,0 +1,13 @@
+package search;
+
+public class Search {
+	public int linearSearch(int[] array, int target) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == target) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+}

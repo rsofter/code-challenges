836b03f4d2a68d50c0251da1a1e083ee0b0575ba
Aleksandr Matlin <ratingsoft@gmail.com>
2021-01-02 18:05:02 +0100

    Removed unused imports.

--- src/strings/StringUtils.java
+++ src/strings/StringUtils.java
@@ -1,14 +1,11 @@
 package strings;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.Stack;
 
 public class StringUtils {
 	public static int countVowels(String str) {
